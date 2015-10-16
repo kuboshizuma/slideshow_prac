e72f27cf25a0a30e0fa82ca043940e1ea8a471c8
kubo shizuma <forothers.295@gmail.com>
2015-10-16 10:21:19 +0900

    fix js file for start

--- slideshow.js
+++ slideshow.js
@@ -1,15 +1,3 @@
-function moveImage(mainNum, files) {
-  $('#main img').attr('src', files[mainNum]);
-  $('.thumbnail').eq(mainNum).addClass('current').siblings().removeClass('current');
-}
-
-function autoPlay() {
-  $('#next').click();
-  timer = setTimeout(function(){
-    autoPlay();
-  }, 2000);
-}
-
 $(function () {
   var files = [
   'images/image1.jpg',
@@ -18,8 +6,6 @@ $(function () {
   'images/image4.jpg'
   ];
   var img;
-  var mainNum = 0;
-  var nowPlaying = false;
   for (var i = 0; i < files.length; i++) {
     img = $('<img>').attr('src', files[i]).addClass('thumbnail');
     $('#thumbnails').append(img);
@@ -30,31 +16,11 @@ $(function () {
 
   $('.thumbnail').eq(0).addClass('current');
 
-  $('.thumbnail').on('click', function() {
-    mainNum = $(this).index();
-    moveImage(mainNum ,files);
-  });
+  //1. previewをクリックしたときの処理
 
-  $('#prev').on('click', function() {
-    mainNum = (mainNum <= 0) ? files.length - 1 : mainNum - 1;
-    moveImage(mainNum ,files);
-  });
+  //2. prev、nextボタンをクリックしたときの処理
 
-  $('#next').on('click', function() {
-    mainNum = (mainNum >= files.length - 1) ? 0 : mainNum + 1;
-    moveImage(mainNum ,files);
-  });
-
-  $('#play').on('click', function() {
-    if (nowPlaying) { return; }
-    nowPlaying = true;
-    autoPlay();
-  });
-
-  $('#stop').on('click', function() {
-    clearTimeout(timer);
-    nowPlaying = false;
-  });
+  //3. 開始、停止ボタンをクリックしたときの処理
 
 });
 
